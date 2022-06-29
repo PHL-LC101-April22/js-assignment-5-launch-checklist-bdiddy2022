9d1a3716728478e8574aadc1a721cace0bed7a94
bdiddy2022 <brendan.t.downs@gmail.com>
2022-06-29 00:07:01 -0400

    EMPTY STRING PREVENTDEFAULT ACHIEVED

--- script.js
+++ script.js
@@ -14,14 +14,33 @@ window.addEventListener("load", function() {
    })
 
 });
+
+
 window.addEventListener("load", function() {
-
-
 let form = document.querySelector("form");
+
 form.addEventListener("submit", function(event) {
-    
-    alert("All fields are required");
-    event.preventDefault();
-    
+    let pilotName = document.querySelector("input[name=pilotName]");
+    let copilotName = document.querySelector("input[name=copilotName]");
+    let fuelLevel = document.querySelector("input[name=fuelLevel]");
+    let cargoMass = document.querySelector("input[name=cargoMass]");
+    console.log(pilotName.value,copilotName.value, fuelLevel.value, cargoMass.value)
+    console.log(Number(pilotName.value), Number(copilotName.value), Number(fuelLevel.value), Number(cargoMass.value))
+    console.log("goodbye");
+    if(pilotName.value===""||copilotName.value===""||fuelLevel.value===""||cargoMass.value===""){
+        console.log("empty trigger");
+        alert("All fields are required");
+        event.preventDefault();
+    }
+    else if(Number(pilotName.value)!="NaN"){
+        console.log("type trigger");
+        alert("Please use letters for names, numbers for levels");
+        event.preventDefault();
+    }
+    else{
+        console.log("ITWORKED");
+    }
+
 });
+
 });
